4127443c8be454253c83a3f98e947e6becd45d51
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2026-03-06 08:07:52 +0500

    Added routes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+const routes = require("./routes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/", routes);
 
 app.get("/", (req, res) => {
     res.send("Aegis VPN Backend Running");

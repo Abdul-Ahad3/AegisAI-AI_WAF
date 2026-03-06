8ad70424e16f9ec0d8dd5c1f0d3cdcf5808fccb5
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2026-03-06 08:18:26 +0500

    Added peer editing logic

--- wgService.js
+++ wgService.js
@@ -13,3 +13,50 @@ exports.generateKeys = async () => {
 
     return { privateKey, publicKey };
 };
+
+exports.createPeer = async (id, publicKey, ip) => {
+
+    const peerConfig = `
+
+# ${id}
+[Peer]
+PublicKey = ${publicKey}
+AllowedIPs = ${ip}/32
+`;
+
+    fs.appendFileSync(WG_CONFIG, peerConfig);
+
+    execSync("wg syncconf wg0 <(wg-quick strip wg0)");
+
+    const serverPublicKey = execSync("wg show wg0 public-key")
+        .toString()
+        .trim();
+
+    const clientConfig = `
+[Interface]
+PrivateKey = CLIENT_PRIVATE_KEY
+Address = ${ip}/24
+DNS = 1.1.1.1
+
+[Peer]
+PublicKey = ${serverPublicKey}
+Endpoint = YOUR_PUBLIC_IP:51820
+AllowedIPs = 0.0.0.0/0
+PersistentKeepalive = 25
+`;
+
+    return clientConfig;
+};
+
+exports.removePeer = async (id) => {
+
+    let config = fs.readFileSync(WG_CONFIG).toString();
+
+    const regex = new RegExp(`# ${id}[\\s\\S]*?AllowedIPs.*`, "g");
+
+    config = config.replace(regex, "");
+
+    fs.writeFileSync(WG_CONFIG, config);
+
+    execSync("wg syncconf wg0 <(wg-quick strip wg0)");
+};
